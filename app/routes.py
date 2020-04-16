a33853627ab9d614f07730d50c8963f620a6b8b2
kairick <kairickaav@gmail.com>
2020-04-16 17:03:51 +0300

    refactoring
    change project structure

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
