555727bac04fa2484c458e8ffc858e43f6ef2d73
kairick <kairickaav@gmail.com>
2020-04-27 11:38:59 +0300

    db first migration
    add order base, change phone field type

--- app/models.py
+++ app/models.py
@@ -7,5 +7,5 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer = db.Column(db.String, index=True)
     address = db.Column(db.String, index=True)
-    phone = db.Column(db.Integer, index=True)
+    phone = db.Column(db.BIGINT, index=True)
     eggs_count = db.Column(db.Integer, index=True)
